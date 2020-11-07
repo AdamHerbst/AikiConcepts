c950b461f7317229fb382bcb0c3a8521437239f6
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2020-11-07 17:38:49 -0500

    Added more functionality, refactored code, fixed bugs

--- includes/section-archive.php
+++ includes/section-archive.php
@@ -1,17 +1,25 @@
-<?php if(have_posts() ): while( have_posts() ): the_post();?>
-
+<!-- This is a skeleton of how the archived blog posts should look -->
+<?php if( have_posts() ): while( have_posts() ): the_post();?>
+	<!-- As with the other sections the following is a different way of displaying the archived posts -->
 	<div class="card mb-3">
+		<!-- Defines as a card layout to display the appropriate information -->
+		<div class="card-body d-flex justify-content-center align-items-center">
+			<?php if(has_post_thumbnail()):?>
 
-		<div class="card-body">
+				<img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail mr-4">
 
-	<!-- Puts title in loop to make every post's title show up-->
-	<h3><?php the_title(); ?></h3>
-	<!-- purs only a portion of the post-->
-	<?php the_excerpt();?>
-	
-	<a href="<?php the_permalink();?>" class="btn btn-success">Read More</a>
-	</div>
-	
+			<?php endif;?>
+			<!-- Inserts the content when this template is used -->
+			<div class="blog-content">
+				<!-- Inserts the wordpress archived post title and an excerpt of the post if longer than a certain number of characters -->
+				<h3><?php the_title();?></h3>
+				<?php the_excerpt();?>
+
+				<a href="<?php the_permalink();?>" class="btn btn-success">Read more</a>
+
+			</div>
+		</div>
 	</div>
 
 <?php endwhile; else: endif;?>
+
--- includes/section-blogcontent.php
+++ includes/section-blogcontent.php
@@ -1,35 +1,35 @@
-<!-- Retrieves all posts to display them -->
-<?php if(have_posts() ): while( have_posts() ): the_post();?>
-
-<!-- Retrieves date of post -->
-	<?php echo get_the_date();?>
-	
-<!-- Retrieves the author's first and last name for display on a post -->
+<!-- The logic of the content for the blogs is below -->
+<!-- Makes sure to display posts only if there are any that exist -->
+<?php if( have_posts() ): while( have_posts() ): the_post();?>
+	<!-- Puts the date in -->
+	<p><?php echo get_the_date('l jS F, Y');?></p>
+	<!-- Places the content from wordpress user below date -->
+	<?php the_content();?>
+	<!-- Inserts User first and last name if present -->
 	<?php 
-	$fname = get_the_author_meta('first_name');
-	$lname = get_the_author_meta('last_name');
+		$fname = get_the_author_meta('first_name');
+		$lname = get_the_author_meta('last_name');
 	?>
-<!-- Puts "posted by: first name last name in top of post -->
-	<p>Posted by: <?php echo $fname . ' '. $lname;?></p>
-<!--
-	The following will implement tags for the posts - Bug/issue - no tags generates an error
--->
-<!--
-	This code does not work correctly right now.
+	<p>Posted by <?php echo $fname;?> <?php echo $lname;?></p>
+	<!-- Retrieves the tags inputted by wordpress user for blog post -->
+	<?php
+		$tags = get_the_tags();
+			if($tags):
+			foreach($tags as $tag):?>
+			<a href="<?php echo get_tag_link($tag->term_id);?>" class="badge badge-success">
+				<?php echo $tag->name;?>
+			</a>
+	<?php endforeach; endif;?>
+
 	<?php 
-	$tags = get_the_tags();
-	foreach($tags as $tag):?>
-	
-		<a href="<?php echo get_tag_link($tag->term_id);?>">
-			<?php echo $tag->name;?>
-		</a>
-		
-		<?php endforeach;?>
--->
-	
-	
-<!-- Puts the content of the post on the correct web page -->
-		<?php the_content();?>
-	
+		$categories = get_the_category();
+			foreach($categories as $cat):?>
+			<a href="<?php echo get_category_link($cat->term_id);?>">
+				<?php echo $cat->name;?>
+			</a>
+	<?php endforeach;?>
+				<!-- Comments need to be implemented -->
+	<?php // comments_template();?>
 
 <?php endwhile; else: endif;?>
+
