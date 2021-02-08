9d462fc41a47883bf6c317cf0ef4cfe0c8c0238a
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-02-08 15:41:47 -0600

    deprecated

--- js/custom.js
+++
@@ -1,9 +0,0 @@
-jQuery('.gallery').each(function() { // the containers for all your galleries
-    jQuery(this).magnificPopup({
-        delegate: 'a', // the selector for gallery item
-        type: 'image',
-        gallery: {
-          enabled:true
-        }
-    });
-});
