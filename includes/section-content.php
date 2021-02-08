97624c4352b71bb99b919ce0e72084bee7d67454
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-02-08 15:42:05 -0600

    deprecated

--- includes/section-content.php
+++
@@ -1,7 +0,0 @@
-<!-- This script retrieves all the posts made in the blog and the content applicable to each -->
-<?php if( have_posts() ): while( have_posts() ): the_post();?>
-
-	<?php the_content();?>
-
-<?php endwhile; else: endif;?>
-
