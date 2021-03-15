0eeb6d2a4baaf4809293eab7135287a8c7e81892
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-03-15 14:24:48 -0500

    Added content and contact form

--- README.md
+++
@@ -1,2 +0,0 @@
-# AikiConcepts
-For my senior Project in CIS 482 - A website for the Aikido Dojo Aikiconcepts
--- index.php
+++ index.php
@@ -1,35 +1,29 @@
+<!--/**************************************************************************
+* Copyright 2020 Adam Herbst*
+* *
+* Licensed under the Apache License, Version 2.0 (the "License"); *
+* you may not use this file except in compliance with the License. *
+* You may obtain a copy of the License at *
+* *
+* http://www.apache.org/licenses/LICENSE-2.0 *
+* *
+* Unless required by applicable law or agreed to in writing, software *
+* distributed under the License is distributed on an "AS IS" BASIS, *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+* See the License for the specific language governing permissions and *
+* limitations under the License. *
+**************************************************************************/ -->
 
-<!DOCTYPE html>  
- <html>  
-      <head>  
-    
-           <title>Attack Plan Day Planner</title>  
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
-           <link rel="stylesheet" type="text/css" href="main.css">
-        
-        </head>  
-      <body>  
-           <br /><br />  
-           <div class="container" style="width:500px;" id="content">  
-
-           <img src="https://images.adamherbst.com/logo_1_AP.png" alt="Logo" class="center">
-
-               <br>
-                <h3 align="center"><b>Attack Plan is a simple day planner that 
-                    intuitively keeps track of past events, current events and future events.</h3>  
-                </b><br/>  
-               
-
-
-                <a href="login.php" class="btn btn-info btn-block" role="button">Login</a>
-                <br>
-                <a href="register.php" class="btn btn-info btn-block" role="button">Register</a>
-
-  
-               
-            
-           </div>  
-      </body>  
- </html>  
+<!-- Loads front page content -->
+<?php get_header(); ?>
+      <div class="row">
+        <div class="col-sm-12 blog-main text-center">
+          <?php if(have_posts()) : ?>
+            <?php while(have_posts()) : the_post(); ?>
+              <?php get_template_part('content', get_post_format()); ?>
+          <?php endwhile; ?>
+        <?php else : ?>
+          <p><?php __('No Posts Found'); ?></p>
+        <?php endif; ?>
+        </div><!-- blog-main -->
+<?php get_footer(); ?>
