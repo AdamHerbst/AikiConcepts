f3e973e701cf43e15c4b5e9d5e7e73d60f9c0c4b
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-03-31 18:34:28 -0500

    Update archive.php

--- archive.php
+++ archive.php
@@ -15,20 +15,28 @@
 **************************************************************************/ -->
 
 <!-- Stylizes archived blog posts -->
-<?php get_header(); ?>
+<?php 
+      get_header(); 
+?>
       <div class="row">
         <div class="col-sm-12 blog-main">
-          <?php if (have_posts()): ?>
+          <?php 
+              if (have_posts()): 
+           ?>
             <?php while (have_posts()):
-        the_post(); ?>
-          <?php get_template_part('content', get_post_format()); ?>
-          <?php
-    endwhile; ?>
+             the_post();
+             ?>
+                   <?php get_template_part('content', get_post_format()); ?>
+             <?php
+               endwhile; 
+              ?>
         <?php
-else: ?>
+            else: 
+        ?>
           <p><?php __('No Posts Found'); ?></p>
         <?php
-endif; ?>
+            endif; 
+         ?>
         </div><!-- blog-main -->
     <?php get_footer(); ?>
 </div>
