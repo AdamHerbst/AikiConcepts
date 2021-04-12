b10ef191c0d845dd297274133b943c22a26617a8
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-04-12 18:08:10 -0500

    Issue #13

--- archive.php
+++ archive.php
@@ -16,19 +16,18 @@
 
 <!-- Stylizes archived blog posts -->
 <?php get_header(); ?>
-      <div class="row">
-        <div class="col-sm-12 blog-main">
-          <?php if (have_posts()): ?>
-            <?php while (have_posts()):
-        the_post(); ?>
-          <?php get_template_part('content', get_post_format()); ?>
-          <?php
-    endwhile; ?>
-        <?php
-else: ?>
-          <p><?php __('No Posts Found'); ?></p>
-        <?php
-endif; ?>
-        </div><!-- blog-main -->
-    <?php get_footer(); ?>
+<div class="row">
+      <div class="col-sm-12 blog-main">
+            <?php 
+                  if (have_posts()):
+                        while (have_posts()):
+                              the_post();
+                              get_template_part('content', get_post_format());
+                        endwhile;
+                  else: 
+                        ?><p><?php __('No Posts Found'); ?></p> <?php
+                  endif; 
+            ?>
+      </div><!-- blog-main -->
+      <?php get_footer(); ?>
 </div>
