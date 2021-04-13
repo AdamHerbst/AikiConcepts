76585154ea9e417d44be2a3a0ff3534a8cdde2b7
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-04-12 20:26:19 -0400

    Issue #16

--- single.php
+++ single.php
@@ -17,13 +17,17 @@
 <!-- Single Blog post Template -->
 <?php get_header(); ?>
       <div class="row">
-        <div class="col-sm-12 blog-main text-center">
-          <?php if(have_posts()) : ?>
-            <?php while(have_posts()) : the_post(); ?>
-            <?php get_template_part('content', get_post_format()); ?>
-          <?php endwhile; ?>
-        <?php else : ?>
-          <p><?php __('No Posts Found'); ?></p>
-        <?php endif; ?>
-        </div><!-- blog-main -->
-    <?php get_footer(); ?>
+            <div class="col-sm-12 blog-main text-center">
+                  <?php if (have_posts()): ?>
+                  <?php while (have_posts()):
+                        the_post(); ?>
+                  <?php get_template_part('content', get_post_format()); ?>
+                  <?php
+                      endwhile; ?>
+                        <?php
+                        else: ?>
+                              <p><?php __('No Posts Found'); ?></p>
+                        <?php endif; ?>
+            </div><!-- blog-main -->
+      </div> <!-- row -->
+<?php get_footer(); ?>
