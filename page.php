440c667347b42d8b2a80b432a937a75636b2230c
Adam Herbst <12887762+AdamHerbst@users.noreply.github.com>
2021-04-12 20:20:44 -0400

    Issue #15

--- page.php
+++ page.php
@@ -17,23 +17,22 @@
 <!-- Default page Template -->
 <?php get_header(); ?>
       <div class="row">
-        <div class="col-sm-12 blog-main">
-          <?php if (have_posts()): ?>
-            <?php while (have_posts()):
-        the_post(); ?>
-          <div class="blog-post">
-            <h2 class="blog-post-title">
-                <?php the_title(); ?>
-            </h2>
-            <?php the_content(); ?>
-          </div><!-- blog-post -->
-          <?php
-    endwhile; ?>
-        <?php
-else: ?>
-          <p><?php __('No Page Found'); ?></p>
-        <?php
-endif; ?>
-        </div><!-- blog-main -->
-    <?php get_footer(); ?>
+            <div class="col-sm-12 blog-main">
+                  <?php if (have_posts()): ?>
+                  <?php while (have_posts()):
+                        the_post(); ?>
+                        <div class="blog-post">
+                              <h2 class="blog-post-title">
+                              <?php the_title(); ?>
+                              </h2>
+                              <?php the_content(); ?>
+                        </div><!-- blog-post -->
+                  <?php endwhile; ?>
+                  <?php else: ?>
+      <p><?php __('No Page Found'); ?></p>
+                  <?php endif; ?>
+            </div><!-- blog-main -->
+      </div> <!-- row -->
+<?php get_footer(); ?>
+         
 
